7094dfcba460e774d52c18330bafe67ab59192d5
yuraymond5 <ryu5@uw.edu>
2025-10-24 13:30:01 -0700

    lift slippery + reduced complexity

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/dexsuite/config/kuka_allegro/dexsuite_kuka_allegro_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/dexsuite/config/kuka_allegro/dexsuite_kuka_allegro_env_cfg.py
@@ -5,6 +5,8 @@
 
 from isaaclab_assets.robots import KUKA_ALLEGRO_CFG
 
+from isaaclab.controllers.differential_ik_cfg import DifferentialIKControllerCfg
+from isaaclab.envs.mdp.actions.actions_cfg import DifferentialInverseKinematicsActionCfg
 from isaaclab.managers import ObservationTermCfg as ObsTerm
 from isaaclab.managers import RewardTermCfg as RewTerm
 from isaaclab.managers import SceneEntityCfg
@@ -17,7 +19,31 @@ from ... import mdp
 
 @configclass
 class KukaAllegroRelJointPosActionCfg:
+    """Original joint position action for both arm and hand (legacy)."""
     action = mdp.RelativeJointPositionActionCfg(asset_name="robot", joint_names=[".*"], scale=0.1)
+
+
+@configclass
+class KukaAllegroIKActionCfg:
+    """IK control for arm and joint position control for hand."""
+
+    # Use IK delta control for arm (iiwa7 joints)
+    arm_action = DifferentialInverseKinematicsActionCfg(
+        asset_name="robot",
+        joint_names=["iiwa7_joint.*"],
+        body_name="palm_link",  # Use Allegro hand palm as the end effector for IK
+        controller=DifferentialIKControllerCfg(command_type="pose", use_relative_mode=True, ik_method="dls"),
+        scale=0.5,
+        body_offset=DifferentialInverseKinematicsActionCfg.OffsetCfg(pos=[0.0, 0.0, 0.0]),
+    )
+
+    # Allegro hand uses EMA joint position control for smoothness
+    hand_action = mdp.EMAJointPositionToLimitsActionCfg(
+        asset_name="robot",
+        joint_names=["index_joint.*", "middle_joint.*", "ring_joint.*", "thumb_joint.*"],
+        alpha=0.95,
+        rescale_to_limits=True,
+    )
 
 
 @configclass
@@ -117,12 +143,30 @@ class DexsuiteKukaAllegroReorientSlipperyEnvCfg_PLAY(KukaAllegroMixinCfg, dexsui
 
 @configclass
 class DexsuiteKukaAllegroLiftEnvCfg(KukaAllegroMixinCfg, dexsuite.DexsuiteLiftEnvCfg):
-    pass
+    """Kuka Allegro lift task with single easy object"""
+
+    def __post_init__(self):
+        super().__post_init__()
+
+        # Use only a single easy object (medium sphere)
+        from isaaclab.sim import SphereCfg, RigidBodyMaterialCfg
+        self.scene.object.spawn.assets_cfg = [
+            SphereCfg(radius=0.05, physics_material=RigidBodyMaterialCfg(static_friction=0.5)),
+        ]
 
 
 @configclass
 class DexsuiteKukaAllegroLiftEnvCfg_PLAY(KukaAllegroMixinCfg, dexsuite.DexsuiteLiftEnvCfg_PLAY):
-    pass
+    """Kuka Allegro lift task with single easy object (play mode)"""
+
+    def __post_init__(self):
+        super().__post_init__()
+
+        # Use only a single easy object (medium sphere)
+        from isaaclab.sim import SphereCfg, RigidBodyMaterialCfg
+        self.scene.object.spawn.assets_cfg = [
+            SphereCfg(radius=0.05, physics_material=RigidBodyMaterialCfg(static_friction=0.5)),
+        ]
 
 
 @configclass
@@ -131,6 +175,12 @@ class DexsuiteKukaAllegroLiftSlipperyEnvCfg(KukaAllegroMixinCfg, dexsuite.Dexsui
 
     def __post_init__(self):
         super().__post_init__()
+
+        # Use only a single easy object (medium sphere)
+        from isaaclab.sim import SphereCfg, RigidBodyMaterialCfg
+        self.scene.object.spawn.assets_cfg = [
+            SphereCfg(radius=0.05, physics_material=RigidBodyMaterialCfg(static_friction=0.5)),
+        ]
 
         # Make only the objects more slippery (reduce friction)
         self.events.object_physics_material.params["static_friction_range"] = [0.05, 0.2]
@@ -154,6 +204,132 @@ class DexsuiteKukaAllegroLiftSlipperyEnvCfg_PLAY(KukaAllegroMixinCfg, dexsuite.D
 
     def __post_init__(self):
         super().__post_init__()
+
+        # Use only a single easy object (medium sphere)
+        from isaaclab.sim import SphereCfg, RigidBodyMaterialCfg
+        self.scene.object.spawn.assets_cfg = [
+            SphereCfg(radius=0.05, physics_material=RigidBodyMaterialCfg(static_friction=0.5)),
+        ]
+
+        # Make only the objects more slippery (reduce friction)
+        self.events.object_physics_material.params["static_friction_range"] = [0.05, 0.2]
+        self.events.object_physics_material.params["dynamic_friction_range"] = [0.05, 0.2]
+
+        # Also reduce the base object friction (defined in scene)
+        for asset in self.scene.object.spawn.assets_cfg:
+            asset.physics_material.static_friction = 0.1
+            asset.physics_material.dynamic_friction = 0.1
+
+        # Make objects heavier (increase base mass from 0.2 kg to 0.4 kg)
+        self.scene.object.spawn.mass_props.mass = 0.4
+
+        # Increase mass randomization range (from [0.2, 2.0] to [1.0, 2.5] scale)
+        self.events.object_scale_mass.params["mass_distribution_params"] = [1.0, 2.5]
+
+
+##
+# IK-based action environments
+##
+
+
+@configclass
+class KukaAllegroIKMixinCfg:
+    """Mixin for Kuka Allegro with IK arm control and joint position hand control."""
+    rewards: KukaAllegroReorientRewardCfg = KukaAllegroReorientRewardCfg()
+    actions: KukaAllegroIKActionCfg = KukaAllegroIKActionCfg()
+
+    def __post_init__(self: dexsuite.DexsuiteReorientEnvCfg):
+        super().__post_init__()
+        self.commands.object_pose.body_name = "palm_link"
+        self.scene.robot = KUKA_ALLEGRO_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
+        finger_tip_body_list = ["index_link_3", "middle_link_3", "ring_link_3", "thumb_link_3"]
+        for link_name in finger_tip_body_list:
+            setattr(
+                self.scene,
+                f"{link_name}_object_s",
+                ContactSensorCfg(
+                    prim_path="{ENV_REGEX_NS}/Robot/ee_link/" + link_name,
+                    filter_prim_paths_expr=["{ENV_REGEX_NS}/Object"],
+                ),
+            )
+        self.observations.proprio.contact = ObsTerm(
+            func=mdp.fingers_contact_force_b,
+            params={"contact_sensor_names": [f"{link}_object_s" for link in finger_tip_body_list]},
+            clip=(-20.0, 20.0),  # contact force in finger tips is under 20N normally
+        )
+        self.observations.proprio.hand_tips_state_b.params["body_asset_cfg"].body_names = ["palm_link", ".*_tip"]
+        self.rewards.fingers_to_object.params["asset_cfg"] = SceneEntityCfg("robot", body_names=["palm_link", ".*_tip"])
+
+
+@configclass
+class DexsuiteKukaAllegroLiftIKEnvCfg(KukaAllegroIKMixinCfg, dexsuite.DexsuiteLiftEnvCfg):
+    """Kuka Allegro lift task with IK arm control and single easy object"""
+
+    def __post_init__(self):
+        super().__post_init__()
+
+        # Use only a single easy object (medium sphere)
+        from isaaclab.sim import SphereCfg, RigidBodyMaterialCfg
+        self.scene.object.spawn.assets_cfg = [
+            SphereCfg(radius=0.05, physics_material=RigidBodyMaterialCfg(static_friction=0.5)),
+        ]
+
+
+@configclass
+class DexsuiteKukaAllegroLiftIKEnvCfg_PLAY(KukaAllegroIKMixinCfg, dexsuite.DexsuiteLiftEnvCfg_PLAY):
+    """Kuka Allegro lift task with IK arm control and single easy object (play mode)"""
+
+    def __post_init__(self):
+        super().__post_init__()
+
+        # Use only a single easy object (medium sphere)
+        from isaaclab.sim import SphereCfg, RigidBodyMaterialCfg
+        self.scene.object.spawn.assets_cfg = [
+            SphereCfg(radius=0.05, physics_material=RigidBodyMaterialCfg(static_friction=0.5)),
+        ]
+
+
+@configclass
+class DexsuiteKukaAllegroLiftSlipperyIKEnvCfg(KukaAllegroIKMixinCfg, dexsuite.DexsuiteLiftEnvCfg):
+    """Kuka Allegro lift task with IK arm control, slippery and heavy objects"""
+
+    def __post_init__(self):
+        super().__post_init__()
+
+        # Use only a single easy object (medium sphere)
+        from isaaclab.sim import SphereCfg, RigidBodyMaterialCfg
+        self.scene.object.spawn.assets_cfg = [
+            SphereCfg(radius=0.05, physics_material=RigidBodyMaterialCfg(static_friction=0.5)),
+        ]
+
+        # Make only the objects more slippery (reduce friction)
+        self.events.object_physics_material.params["static_friction_range"] = [0.05, 0.2]
+        self.events.object_physics_material.params["dynamic_friction_range"] = [0.05, 0.2]
+
+        # Also reduce the base object friction (defined in scene)
+        for asset in self.scene.object.spawn.assets_cfg:
+            asset.physics_material.static_friction = 0.1
+            asset.physics_material.dynamic_friction = 0.1
+
+        # Make objects heavier (increase base mass from 0.2 kg to 0.4 kg)
+        self.scene.object.spawn.mass_props.mass = 0.4
+
+        # Increase mass randomization range (from [0.2, 2.0] to [1.0, 2.5] scale)
+        self.events.object_scale_mass.params["mass_distribution_params"] = [1.0, 2.5]
+
+
+@configclass
+class DexsuiteKukaAllegroLiftSlipperyIKEnvCfg_PLAY(KukaAllegroIKMixinCfg, dexsuite.DexsuiteLiftEnvCfg_PLAY):
+    """Kuka Allegro lift task with IK arm control, slippery and heavy objects (play mode)"""
+
+    def __post_init__(self):
+        super().__post_init__()
+
+        # Use only a single easy object (medium sphere)
+        from isaaclab.sim import SphereCfg, RigidBodyMaterialCfg
+        self.scene.object.spawn.assets_cfg = [
+            SphereCfg(radius=0.05, physics_material=RigidBodyMaterialCfg(static_friction=0.5)),
+        ]
 
         # Make only the objects more slippery (reduce friction)
         self.events.object_physics_material.params["static_friction_range"] = [0.05, 0.2]
